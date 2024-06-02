199a088211d15e3d774ccfc02a20c6efa89903ac
Unyc1124 <aditidubey792@gmail.com>
2024-06-02 13:46:03 +0530

    heaps

---
+++ 35.Heaps/PeekHeap.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PeekHeap {
+    static class Heap{
+        ArrayList<Integer> arr= new ArrayList<>();
+
+        public void add(int data){
+            //add at the last idx
+            arr.add(data);
+            int x = arr.size()-1;
+
+            int paridx=(x-1)/2;//parent index
+
+            while(arr.get(x) < arr.get(paridx)){//)(log n)
+                //swap
+                int temp = arr.get(x);
+                arr.set(x,arr.get(paridx));
+                arr.set(paridx,temp);
+                x=paridx;
+                paridx=(x-1)/2;
+            }
+        }
+
+        public int peek(){
+            return arr.get(0);
+        }
+    }
+    public static void main(String args[]){
+
+    }
+}

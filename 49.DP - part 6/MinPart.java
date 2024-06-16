5a6e355500e62b49b4567005c8f478b8d3b4d9eb
Unyc1124 <aditidubey792@gmail.com>
2024-06-16 20:35:01 +0530

    update

--- 49.DP - part 6/MinPart.java
+++ 49.DP - part 6/MinPart.java
@@ -1,3 +1,38 @@
-public class MinPart {//minimum partitioning
-    
+public class MinPart {//minimum partitioning-0 1 knapsack variation
+    public static void main(String args[]){//O(n*W)
+        int nums[]={1,6,11,5};
+
+        System.out.println(minPart(nums));
+
+    }
+
+    public static int minPart(int nums[]){
+        int n=nums.length;
+        int sum =0;
+
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+        }
+
+        int W = sum /2;
+
+        int dp[][] = new int[n+1][W+1];
+
+        //bottom up
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                if(nums[i-1]<=j){//va;id
+                    //include
+                    dp[i][j]=Math.max(nums[i-1] + dp[i-1][j-nums[i-1]],dp[i-1][j]);
+                
+                }else{
+                    //invalid
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum2 - sum1);
+    }
 }
